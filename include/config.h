45131a880e8b5e65ff1fc826f9ccead270c2f4c0
rmenai <rami.menai@outlook.com>
2021-11-17 20:18:08 +0300

    Configure cmake and conan

---
+++ include/config.h
@@ -0,0 +1,8 @@
+#ifndef CPP_CONFIG_H
+#define CPP_CONFIG_H
+
+namespace Settings {
+
+}
+
+#endif //CPP_CONFIG_H
---
+++ tests/test_add.cpp
@@ -0,0 +1,14 @@
+//
+// This is a generated example file that is supposed to be modified.
+//
+
+#include <gtest/gtest.h>
+
+int add(int i, int j) {
+    return i + j;
+}
+
+TEST(test_add, BasicAssertions) {
+    // Expect equality.
+    EXPECT_EQ(add(2, 2), 4);
+}
